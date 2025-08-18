c156ebc41f7c1baa3010743a8133ca0113251ad1
Thien An Bui <thienan2050@gmail.com>
2025-08-18 18:00:23 +0700

    Enable backend LOG via RTTd

--- src/sdk_config.h
+++ src/sdk_config.h
@@ -8120,15 +8120,15 @@
 // <e> NRF_LOG_ENABLED - nrf_log - Logger
 //==========================================================
 #ifndef NRF_LOG_ENABLED
-  #define NRF_LOG_ENABLED 0
+  #define NRF_LOG_ENABLED 1
 #endif
 
 #ifndef NRF_LOG_BACKEND_RTT_ENABLED
-  #define NRF_LOG_BACKEND_RTT_ENABLED 0
+  #define NRF_LOG_BACKEND_RTT_ENABLED 1
 #endif
 
 #ifndef NRF_LOG_BACKEND_SERIAL_USES_RTT
-  #define NRF_LOG_BACKEND_SERIAL_USES_RTT 0
+  #define NRF_LOG_BACKEND_SERIAL_USES_RTT 1
 #endif
 // <h> Log message pool - Configuration of log message pool
 
@@ -8197,7 +8197,7 @@
 // <4=> Debug
 
 #ifndef NRF_LOG_DEFAULT_LEVEL
-  #define NRF_LOG_DEFAULT_LEVEL 3
+  #define NRF_LOG_DEFAULT_LEVEL 4
 #endif
 
 // <q> NRF_LOG_DEFERRED  - Enable deferred logger.
